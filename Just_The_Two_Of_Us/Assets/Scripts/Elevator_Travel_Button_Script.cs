7caf7cabbb56e92f2b7c698bae87df7fff20b024
17611352 <jadentcloete454@gmail.com>
2021-10-07 10:57:13 +0200

    Updated Mechanics & level design
    -Desert Level Added.
    
    -Added a puzzle that will open a door in future versions. This door leads to the dead scientists secret lab where a high powered laser can be found. Which then will allow the player to enter the blast door which will also be added in future builds.

---
+++ Just_The_Two_Of_Us/Assets/Scripts/Elevator_Travel_Button_Script.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator_Travel_Button_Script : MonoBehaviour
+{
+    public string travelValue = "";
+}
--- Just_The_Two_Of_Us/Assets/Scripts/Event_Manager.cs
+++ Just_The_Two_Of_Us/Assets/Scripts/Event_Manager.cs
@@ -6,6 +6,9 @@ public class Event_Manager : MonoBehaviour
 {
     Game_Manager _Manager;
 
+    [Header("Optional UI Logic")]
+    [SerializeField] Animator fade_BG_Anim;
+
     [Header("ICE DOME LOGIC")]
     [SerializeField] Keypad_Instance keypad_Instance;
     [SerializeField] string keypad_Passcode;
@@ -18,12 +21,21 @@ public class Event_Manager : MonoBehaviour
     [SerializeField] bool doorICEMelted;
     Vector3 doorMeltPos;
 
+    [Header("DESERT DOME LOGIC")]
+    [SerializeField] Animator elevatorTravel;
+    bool travel;
+    string currentLevel = "Up";
+    [SerializeField] bool[] blackhole_Rings_InPos = new bool[5];
+    [SerializeField] int boolCheck = 0;
+
 
     public enum EventScenario
     {
-        ICEBox_1
+        ICEBox_1,
+        DESERTBox_1
     }
 
+    [Header("EVENT LOGIC")]
     public EventScenario @event;
 
     // Start is called before the first frame update
@@ -35,6 +47,7 @@ public class Event_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ICE Dome logic
         if(@event == EventScenario.ICEBox_1)
         {
             //Melt ICE On Door
@@ -55,11 +68,50 @@ public class Event_Manager : MonoBehaviour
                 ElevatorDoorCommand_ICE_DOME(true);
                 setDoorOpen = true;
             }
+        }
 
-
+        //Deserts Dome logic
+        if (@event == EventScenario.DESERTBox_1)
+        {
+            if(boolCheck == blackhole_Rings_InPos.Length)
+            {
+                UnlockLaserItemDoor_DESERT_DOME();
+                Debug.Log("Unlocked");
+            }
         }
     }
 
+
+    public void UpdateRingBoolState(int ringID, bool state)
+    {
+        if(blackhole_Rings_InPos[ringID] == true)
+        {
+            boolCheck--;
+        }
+        else if(state == true)
+        {
+            boolCheck++;
+        }
+
+        blackhole_Rings_InPos[ringID] = state;
+    }
+
+
+    void UnlockLaserItemDoor_DESERT_DOME()
+    {
+
+    }
+
+
+    public void Exit_ICE_DOME()
+    {
+        ElevatorDoorCommand_ICE_DOME(false);
+        setDoorOpen = false;
+
+        @event = EventScenario.DESERTBox_1;
+
+        StartCoroutine(TakePlayerToMainHub());
+    }
 
 
     public void UpdateKeyCardStatus(int keycard_ID ,bool boolState)
@@ -84,7 +136,37 @@ public class Event_Manager : MonoBehaviour
         {
             elevatorDoor.Play("Elevator_Door_Close");
         }
-
     }
 
+
+    public bool ElevatorTravelCommand_DESERT_DOME(string travelDir)
+    {
+        bool returnValue = false;
+
+        if(travelDir == "Up" && currentLevel == "Down")
+        {
+            currentLevel = travelDir;
+            elevatorTravel.Play("Elevator_Travel_Up");
+            returnValue = true;
+        }
+        else if(travelDir == "Down" && currentLevel == "Up")
+        {
+            currentLevel = travelDir;
+            elevatorTravel.Play("Elevator_Travel_Down");
+            returnValue = true;
+        }
+
+        return returnValue;
+    }
+
+
+    IEnumerator TakePlayerToMainHub()
+    {
+        fade_BG_Anim.Play("Fade_In");
+
+        yield return new WaitForSeconds(4f);
+
+        fade_BG_Anim.Play("Fade_Out");
+        _Manager.NewPlayerPosition(_Manager.mainHubSpawnPoint);
+    }
 }
