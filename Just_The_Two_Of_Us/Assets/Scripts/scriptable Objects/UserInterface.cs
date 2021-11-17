e5f44c7ec4b5eb5462599310482c36aabfb233d2
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2021-11-17 15:49:18 +0200

    scriptable User Interface Done

--- Just_The_Two_Of_Us/Assets/Scripts/scriptable Objects/UserInterface.cs
+++ Just_The_Two_Of_Us/Assets/Scripts/scriptable Objects/UserInterface.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class UserInterface : ScriptableObject
 {
-    public int temperature;
+    [Header("UI Values")]
+    public float temperature;
+    public int InvetorySize;
+    public List<Color32> TempgaugeColors;
+    public List<Image> PlayerCondition;
+    public List<Image> InventortyImages;
+    [SerializeField] float health = 100;
+    
+
+    [Header("UI")]
+    [SerializeField] TextMeshProUGUI Interact_Grab_UI;
+    [SerializeField] TextMeshProUGUI Interact_PressButton_UI;
+    [SerializeField] TextMeshProUGUI Health_UI;
+    [SerializeField] RawImage Minimap;
 
 
+    [Header("UI GameObjects")]
+    [SerializeField] GameObject TempratureGauge;
+    [SerializeField] GameObject HealthGauge;
+    [SerializeField] GameObject InteractableField;
 }
+
