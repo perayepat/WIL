7e95fcd015cc26e12d15a237b53887e6a520a5c2
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2021-11-17 16:07:11 +0200

    Materials change

--- Just_The_Two_Of_Us/Assets/Scripts/scriptable Objects/UserInterface.cs
+++ Just_The_Two_Of_Us/Assets/Scripts/scriptable Objects/UserInterface.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
+[CreateAssetMenu(fileName ="New Visor UI", menuName = "Visor UI")]
 public class UserInterface : ScriptableObject
 {
+    public enum snowLevels
+    {
+        NORMAL,
+        COLD,
+        FREEZING
+    }
+    public enum desertLevels
+    {
+        NORMAL,
+        WARM,
+        ARRID
+    }
+
+
     [Header("UI Values")]
     public float temperature;
     public int InvetorySize;
@@ -13,6 +28,8 @@ public class UserInterface : ScriptableObject
     public List<Image> PlayerCondition;
     public List<Image> InventortyImages;
     [SerializeField] float health = 100;
+    public snowLevels sLevels;
+    public desertLevels dLelevels;
     
 
     [Header("UI")]
